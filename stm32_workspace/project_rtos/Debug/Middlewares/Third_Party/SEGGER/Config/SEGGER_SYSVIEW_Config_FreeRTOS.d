55caa80ad633dba523508c4374017ae07f8569c3
thanhtranndang <thanhtranndang@gmail.com>
2024-06-17 21:10:00 +0700

    update

---
+++ stm32_workspace/project_rtos/Debug/Middlewares/Third_Party/SEGGER/Config/SEGGER_SYSVIEW_Config_FreeRTOS.d
@@ -0,0 +1,24 @@
+Middlewares/Third_Party/SEGGER/Config/SEGGER_SYSVIEW_Config_FreeRTOS.o: \
+ ../Middlewares/Third_Party/SEGGER/Config/SEGGER_SYSVIEW_Config_FreeRTOS.c \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h \
+ ../Core/Inc/FreeRTOSConfig.h \
+ D:/STM32F4x/stm32_workspace/project_rtos/Middlewares/Third_Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h \
+ D:/STM32F4x/stm32_workspace/project_rtos/Middlewares/Third_Party/SEGGER/SEGGER/SEGGER_SYSVIEW.h \
+ D:/STM32F4x/stm32_workspace/project_rtos/Middlewares/Third_Party/SEGGER/SEGGER/SEGGER.h \
+ D:/STM32F4x/stm32_workspace/project_rtos/Middlewares/Third_Party/SEGGER/Config/Global.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h:
+../Core/Inc/FreeRTOSConfig.h:
+D:/STM32F4x/stm32_workspace/project_rtos/Middlewares/Third_Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h:
+D:/STM32F4x/stm32_workspace/project_rtos/Middlewares/Third_Party/SEGGER/SEGGER/SEGGER_SYSVIEW.h:
+D:/STM32F4x/stm32_workspace/project_rtos/Middlewares/Third_Party/SEGGER/SEGGER/SEGGER.h:
+D:/STM32F4x/stm32_workspace/project_rtos/Middlewares/Third_Party/SEGGER/Config/Global.h:
+../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h:
+../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h:
+../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h:
+../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h:
---
+++ stm32_workspace/projectfreertos/Debug/Middlewares/Third_Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.d
@@ -0,0 +1,28 @@
+Middlewares/Third_Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.o: \
+ ../Middlewares/Third_Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.c \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h \
+ ../Core/Inc/FreeRTOSConfig.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h \
+ D:/STM32F4x/stm32_workspace/projectfreertos/Middlewares/Third_Party/SEGGER/SEGGER/SEGGER_SYSVIEW.h \
+ D:/STM32F4x/stm32_workspace/projectfreertos/Middlewares/Third_Party/SEGGER/SEGGER/SEGGER.h \
+ D:/STM32F4x/stm32_workspace/projectfreertos/Middlewares/Third_Party/SEGGER/Config/Global.h \
+ ../Middlewares/Third_Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h
+../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h:
+../Core/Inc/FreeRTOSConfig.h:
+../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h:
+../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h:
+../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h:
+../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h:
+../Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
+../Middlewares/Third_Party/FreeRTOS/Source/include/list.h:
+D:/STM32F4x/stm32_workspace/projectfreertos/Middlewares/Third_Party/SEGGER/SEGGER/SEGGER_SYSVIEW.h:
+D:/STM32F4x/stm32_workspace/projectfreertos/Middlewares/Third_Party/SEGGER/SEGGER/SEGGER.h:
+D:/STM32F4x/stm32_workspace/projectfreertos/Middlewares/Third_Party/SEGGER/Config/Global.h:
+../Middlewares/Third_Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h:
